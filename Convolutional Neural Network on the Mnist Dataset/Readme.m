5886c7cb86d0af2ec193d9855121ca1a0b60ad7a
Marios Aspris <aspris.marios@gmail.com>
2017-11-01 09:37:29 +0100

    Update Readme.m

--- Convolutional Neural Network on the Mnist Dataset/Readme.m
+++ Convolutional Neural Network on the Mnist Dataset/Readme.m
@@ -1,4 +1,7 @@
 # Details
-This code builds a Convolutional neural network with 2 convolutional layers, with rectified linear units for activation and two fully connected layers again with rectified linear units for activation. Dropout is also implemnted before the final fully connected layer. Optimization of the cross-entropy function is done with the Adam Optimizer. 
+This code builds a Convolutional neural network with 2 convolutional layers, with rectified linear units for activation 
+and two fully connected layers again with rectified linear units for activation.
+Dropout is also implemnted before the final fully connected layer.
+Optimization of the cross-entropy function is done with the Adam Optimizer. 
 Implementation is done using the tensorflow library.
 Run time on a nVidia 2Gb Gpu is approximately 20 mins(bash timing), with an accuracy around 99.2%. 
